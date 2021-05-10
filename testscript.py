6e87edf90a09a92ec16697588f829f473d2b1beb
Iraj Eshghi <ie355@gr043.nyu.cluster>
2021-05-10 10:04:03 -0400

    bigger test

--- testscript.py
+++ testscript.py
@@ -4,13 +4,13 @@ import numpy as np
 N = 20
 T = 3000*1.38*10**(-23) 
 L = 1e-5
-dt = 1e-6
+dt = 1e-8
 rho = 1000
 mu = 0.01
-Np = 20
-psize = 1e-6
-interac = 1e-15
-maxt = 5*1e-1
+Np = 200
+psize = 1e-7
+interac = 2e-14
+maxt = 5*1e-3
     
 xh,th,tk = code1.main(N,T,L,dt,rho,mu,Np,psize,interac,maxt)
 
